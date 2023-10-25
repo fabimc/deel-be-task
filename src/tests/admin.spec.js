3a9fc9aed1386b936565fc2394e64f632574a373
Fabián Montealegre <fabimc@gmail.com>
2023-10-25 11:14:14 -0600

    adds mocks to tests

--- src/tests/admin.spec.js
+++ src/tests/admin.spec.js
@@ -1,8 +1,12 @@
 const app = require('../app')
+const { profileService } = require('../services')
 const request = require('supertest')
+const bestClientsMock = require('./mocks/bestClients')
 
 describe('GET /admin/best-clients', () => {
   test('should return 200 OK and have the correct shape', async () => {
+    const getBestClients = jest.spyOn(profileService, 'getBestClients').mockReturnValueOnce(bestClientsMock)
+
     await request(app)
       .get('/admin/best-clients?start=1597471200000&end=1597730399000&limit=2')
       .expect(200)
@@ -14,7 +18,7 @@ describe('GET /admin/best-clients', () => {
 
         // Check the response data
         expect(response.body[0].id).toBe(4)
-        expect(response.body[0].fullName).toBe('Ash Kethcum')
+        expect(response.body[0].fullName).toBe('Ash Ketchum')
         expect(response.body[0].paid).toBe(2020)
       })
   })
---
+++ src/tests/mocks/bestClients.js
@@ -0,0 +1,14 @@
+const bestClientsMock = [
+  {
+    id: 4,
+    fullName: 'Ash Ketchum',
+    paid: 2020
+  },
+  {
+    id: 1,
+    fullName: 'Harry Potter',
+    paid: 421
+  }
+]
+
+module.exports = bestClientsMock
