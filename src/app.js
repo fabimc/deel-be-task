c63689281bbb671213d9ad40ec9f25fa20675c53
Fabián Montealegre <fabimc@gmail.com>
2023-10-26 16:27:24 -0600

    adds mustache as template engine

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const path = require('path');
+const path = require('path')
+const Mustache = require('mustache')
+var fs = require('fs')
 const { adminRouter, balanceRouter, contractRouter, jobRouter } = require('./routes')
 
 const app = express()
 app.use(bodyParser.json())
 
 // Health check
-
 app.get('/health', (req, res) => {
   res.status(200).send('OK')
 })
@@ -18,8 +19,12 @@ app.use('/api/balances', balanceRouter)
 app.use('/api/contracts', contractRouter)
 app.use('/api/jobs', jobRouter)
 
+// UI routes
 app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  const page = fs.readFileSync(path.join(__dirname, '/views/index.tpl')).toString()
+  const view = { title: 'Contracts’ Landing Page' }
+  const output = Mustache.render(page, view)
+  res.send(output)
 })
 
 module.exports = app
